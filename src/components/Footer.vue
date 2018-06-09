acf95c3abf333d8ba4bdf9479410512d08a4ed2c
Frank Wei <frank@frankwei.xyz>
2018-06-09 15:33:25 +0800

    feat: Header and Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <el-footer>
+        EOS LBS Lucky Money
+        <br>
+        Andoromeda Foundation All Right Reserved.
+    </el-footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+<el-menu
+ class="el-menu-demo"
+ :default-active="$route.path"
+ mode="horizontal"
+ @select="handleSelect"
+ router>
+  <el-menu-item index="/"> EOS Lucky Money </el-menu-item>
+  <el-menu-item index="/home"> Home </el-menu-item>
+  <el-menu-item index="/send"> Send Lucky Money </el-menu-item>
+  <el-menu-item index="/map"> Maps </el-menu-item>
+</el-menu>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  }),
+  methods: {
+    handleSelect (key, keyPath) {
+      console.info(key, keyPath)
+    }
+  }
+}
+</script>
