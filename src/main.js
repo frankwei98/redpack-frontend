dd730867dfa0d80cd3c13ac9a39640a8344027e4
Frank Wei <frank@frankwei.xyz>
2018-06-09 15:33:15 +0800

    feat: import Element-UI and Amap Lib

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Buefy from 'buefy'
+import VueAMap from 'vue-amap'
+import ElementUI from 'element-ui'
 import App from './App'
 import router from './router'
-import 'buefy/lib/buefy.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(Buefy)
+Vue.use(ElementUI)
+Vue.use(VueAMap)
 
+// 初始化vue-amap
+VueAMap.initAMapApiLoader({
+  // 高德的key
+  key: '2b69fc6bfb4b1c63cc89d7a8054aaa1f',
+  // 插件集合
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 高德 sdk 版本，默认为 1.4.4
+  v: '1.4.4'
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
