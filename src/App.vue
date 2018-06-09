f40891c77fffd19f40fee7ceb66f0f624129bb4c
Frank Wei <frank@frankwei.xyz>
2018-06-09 15:32:50 +0800

    refactor: change app view

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header-component />
     <router-view/>
+    <footer-component/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
+  components: {
+    'header-component': Header,
+    'footer-component': Footer
+  },
   name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
