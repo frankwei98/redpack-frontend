67fe866f7f90b462d1aa651e01d7ed742731f402
Frank Wei <frank@frankwei.xyz>
2018-06-10 14:48:43 +0800

    il18

--- src/views/ClaimLuckyMoney.vue
+++ src/views/ClaimLuckyMoney.vue
@@ -2,7 +2,7 @@
     <div id="claim-lucky-money">
         <h1 class="title">You're claiming #{{id}} red pack</h1>
         <h2 class="subtitle">Message: {{msg}} </h2>
-        <button class="open" @click="openPack">拆</button>
+        <button class="open" @click="openPack">open</button>
     </div>
 </template>
 
@@ -28,23 +28,23 @@ export default {
         const result = await claim(this.id)
         console.log(result)
         if (result.broadcast) {
-          this.$alert('领取成功', '红包领取成功', {
-            confirmButtonText: '确定',
+          this.$alert('success', 'successful claim your red packet', {
+            confirmButtonText: 'confirm',
             callback: action => {
               this.$message({
                 type: 'success',
-                message: `领取操作已经广播，请稍后检查账户余额`
+                message: `you've claimed your red packet, please come back and check your balance latert`
               })
             }
           })
         }
       } catch (error) {
-        this.$alert('时间未到，请确认开始时间', '领取失败', {
-          confirmButtonText: '确定',
+        this.$alert('it is not available right now, please come back and check when it starts', 'failed', {
+          confirmButtonText: 'confirm',
           callback: action => {
             this.$message({
               type: 'error',
-              message: `时候未到`
+              message: `it is not available right now`
             })
           }
         })
