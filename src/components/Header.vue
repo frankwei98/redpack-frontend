b08dacfb3e1bbf91bba93354a0d92ca15a9e4e70
Frank Wei <frank@frankwei.xyz>
2018-06-10 13:37:43 +0800

    fix:fix bug

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,11 +9,6 @@
   <el-menu-item index="/home"> Home </el-menu-item>
   <el-menu-item index="/send"> Send Red Packet </el-menu-item>
   <el-menu-item index="/map"> Maps </el-menu-item>
-<<<<<<< HEAD
-  <!-- <el-menu-item index="/my"> My Red Pack </el-menu-item> -->
-=======
-  <el-menu-item index="/my"> My Red Packet </el-menu-item>
->>>>>>> 39f80fc0584bf734b4a0a85f0b10702ad0d369aa
 </el-menu>
 </template>
 
