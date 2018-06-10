85f6aa5f404223b3fc8b53e93ffebc1dc4b966be
Frank Wei <frank@frankwei.xyz>
2018-06-10 09:24:00 +0800

    fix: replace with my privk and username

--- src/eos/index.js
+++ src/eos/index.js
@@ -4,14 +4,14 @@ import eosJS from 'eosjs'
 
 const privKey = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
 
-let contractName = 'luckpoint.co'
-let accountName = 'banker'
+let contractName = 'redpack'
+let accountName = 'frankweiloca'
 
 const EOSJS_CONFIG = {
-  contractName: contractName, // 合约名字
+  contractName, // 合约名字
   contractSender: accountName, // 执行合约的账户 (需要与私钥对应)
   clientConfig: {
-    keyProvider: ['5JcziTgwUhQgKyvmvc4ygEPGonQPVrBYNTwezAg5UuJ7djyVDWQ'], // 账号对应的私钥
+    keyProvider: privKey, // 账号对应的私钥
     httpEndpoint: 'http://127.0.0.1:8888' // EOS节点程序的http终端
   }
 }
